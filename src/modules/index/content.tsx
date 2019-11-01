e7d9b656fd83d05a1df4a8019b05013d6a55669a
vist wang <>
2019-11-01 09:14:35 +0800

    20191101

--- src/modules/index/content.tsx
+++ src/modules/index/content.tsx
@@ -8,6 +8,21 @@ import LocaleSon from '../../components/contextson';
 import Medemo from '../../components/demo';
 import { ConnectComponent, store } from '../../components/reduxDemo';
 
+// 三种路由
+// 1 url 路由
+// 2 hash路由 支持低版本；浏览器
+// 3 内存路由 url 不变，页面会切换
+
+// 核心 api
+// 1 Link 
+// 2 NavLink 能动态添加 activeClassName="xxx"
+// 3 Prompt 满足条件时候，提示用户是否离开当前页面
+// 4 Redirect 重定向当前页面，例如登录判断
+// 5 Route 路由匹配时候显示对应组件 exact 是否精确匹配 可以多匹配多显示
+// 6 Switch 只显示第一个匹配的路由
+
+// 通过url 传递参数 页面状态尽量通过URL 参数定义
+
 export const enString: IMyLang = {
     submit: 'Submit',
     cancel: 'Cancel'
